0003fe33926746d709c33a6cf0b4a64b35099f76
Luis Soares Brasil <llhenriquesoares@gmail.com>
2024-01-02 15:08:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,33 +2,14 @@
 function playRockPaperScissor(player1,player2){
     let result = '';
 
-    if (player1 == "Pedra"){
-        if (player2 == "Pedra"){
-            result = 'Empate!';
-        }else if (player2 == "Papel"){
-            result = 'Jogador 2 venceu!';
-        }else{
+    if (((player1 == "Pedra")&&(player2 == "Tesoura"))||((player1 == "Papel")&&(player2 == "Pedra"))||((player1 == "Tesoura")&&(player2 == "Papel"))){
             result = 'Jogador 1 venceu!';
-        }
-    }else if (player1 == "Papel"){
-            if (player2 == "Pedra"){
-                result = 'Jogador 1 venceu!';
-            }else if (player2 == "Papel"){
-                result = 'Empate!';
-            }else{
-                result = 'Jogador 2 venceu!';
-            }
+    }else if(((player2 == "Pedra")&&(player1 == "Tesoura"))||((player2 == "Papel")&&(player1 == "Pedra"))||((player2 == "Tesoura")&&(player1 == "Papel"))){
+            result = 'Jogador 2 venceu!';
     }else{
-        if (player2 == "Pedra"){
-            result = 'Jogador 2 venceu!';
-        }else if (player2 == "Papel"){
-            result = 'Jogador 1 venceu!';
-        }else{
             result = 'Empate!';
         }
-    }
 
     return result;
-}
 
-console.log(result);
+}
